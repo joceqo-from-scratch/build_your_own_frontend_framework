7070245d9ad532acc8297f3f7ec5ac86b7265e27
jocelin queau <queau.j@gmail.com>
2025-03-13 22:48:11 +0100

    chapter4

--- packages/runtime/src/mount-dom.js
+++ packages/runtime/src/mount-dom.js
@@ -1,4 +1,6 @@
 import { DOM_TYPES } from './h'
+import { setAttributes } from './attributes'
+import { addEventListeners } from './events'
 
 export function mountDOM(vdom, parentEl) {
   switch (vdom.type) {
@@ -21,4 +23,38 @@ export function mountDOM(vdom, parentEl) {
       throw new Error(`Can't mount DOM of type: ${vdom.type}`)
     }
   }
+}
+
+function createTextNode(vdom, parentEl) {
+  const { value } = vdom
+
+  const textNode = document.createTextNode(value)
+  vdom.el = textNode
+
+  parentEl.append(textNode)
+}
+
+function createFragmentNodes(vdom, parentEl) {
+  const { children } = vdom
+  vdom.el = parentEl
+
+  children.forEach((child) => mountDOM(child, parentEl))
+}
+
+function createElementNode(vdom, parentEl) {
+  const { tag, props, children } = vdom
+
+  const element = document.createElement(tag)
+  addProps(element, props, vdom)
+  vdom.el = element
+
+  children.forEach((child) => mountDOM(child, element))
+  parentEl.append(element)
+}
+
+function addProps(el, props, vdom) {
+  const { on: events, ...attrs } = props
+
+  vdom.listeners = addEventListeners(events, el)
+  setAttributes(el, attrs)
 }
